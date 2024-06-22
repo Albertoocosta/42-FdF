0aadc2d13ace6fa2047ceeb9d82cd168728e25c6
Carlos Alberto Da Fonseca Costa Neto <cda-fons@c1r7s6.42porto.com>
2024-06-22 16:05:20 +0100

    FEAT: improve get_dimensions

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
 CC = cc -g
 NAME = generate
 MLX = minilibx-linux/libmlx.a
+LIBFT = Libft/libft.a
+PRINTF = Printf/libftprintf.a
 FLAGS = -Lmlx_linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz -Wall -Werror -Wextra
 SRCS = fdf.c 
 
@@ -9,12 +11,17 @@ all: ${NAME}
 ${MLX}:
 	make --silent -C minilibx-linux
 
-${NAME}: ${MLX}
-	${CC} ${SRCS} ${MLX} ${FLAGS} -o ${NAME}
+${LIBFT}:
+	make --silent -C Libft
+
+${PRINTF}:
+	make --silent -C Printf
+
+${NAME}: ${MLX} ${LIBFT} ${PRINTF}
+	${CC} ${SRCS} ${MLX} ${LIBFT} ${PRINTF} ${FLAGS} -o ${NAME}
 
 clean:
 	@rm ${NAME}
-
-re: fclean all
+re: clean all
 
 .PHONY : all clean fclean re
--- fdf.c
+++ fdf.c
@@ -6,11 +6,14 @@
 /*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 16:12:43 by cda-fons          #+#    #+#             */
-/*   Updated: 2024/06/18 17:57:59 by cda-fons         ###   ########.fr       */
+/*   Updated: 2024/06/22 15:57:42 by cda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/fdf.h"
+#include "./Libft/libft.h"
+#include "./Printf/ft_printf.h"
+
 
 int	check_map(char *filename)
 {
@@ -18,9 +21,7 @@ int	check_map(char *filename)
 
 	len = ft_strlen(filename);
 	if (filename[len - 4] == '.' && filename[len - 3] == 'f' && filename[len - 2] == 'd' && filename[len - 1] == 'f')
-	{
 		return (1);
-	}
 	else
 	{
 		ft_printf("File not .fdf");
@@ -41,25 +42,32 @@ t_map	*get_dimensions(int fd)
 		ft_printf("Error to allocated map");
 		exit(14);
 	}
-	width = get_next_line(fd);
+	width = ft_strlen(get_next_line(fd));
+	ft_printf("gnl -> %s\n", get_next_line(fd));
+	ft_printf("width antes do loop -> %d\n", width);
 	while (width != 0)
 	{
-		if (get_next_line(fd) == 0)
+		if (get_next_line(fd) == NULL)
 			break ;
-		width = get_next_line(fd);
+		ft_printf("gnl inside loop-> %s\n", get_next_line(fd));
+		width = ft_strlen(get_next_line(fd));
 		height++;
 	}
+	ft_printf("height -> %d\n", height);
 	map->height = height;
 	map->width = width;
 	map->list = NULL;
 	map->z_max = 0;
 	map->z_min = 0;
+	return(map);
 }
 
 int main(int argc, char **argv)
 {
 	int	fd;
+	t_map *map;
 
+	map = NULL;
 	if (argc != 2)
 	{
 		ft_printf("usage: ./fdf <mapname>.fdf");
@@ -71,10 +79,8 @@ int main(int argc, char **argv)
 		ft_printf("Error opening");
 		exit(11);
 	}
-	if (check_map(argv[2]))
-	{
-		get_dimensions(fd);
-	}
+	if (check_map(argv[1]))
+		map = get_dimensions(fd);
 	else
 	{
 		ft_printf("Map error");
